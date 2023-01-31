f615a1755230cc1a2ce6aabe4f0321406c9dad5b
Joinemm <joonas@rautiola.co>
2023-01-31 15:03:14 +0200

    :adhesive_bandage: Fix domain constant on dev environment

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,9 +1,9 @@
 const vercel_public_domain = process.env.NEXT_PUBLIC_DOMAIN;
-const node_prod_env = process.env.NODE_ENV === 'production';
+const vercel_public_url = process.env.NEXT_PUBLIC_VERCEL_URL;
 
 // if Vercel prod deployment, then use main domain; else use temporary domain unless you're on dev
 export const DOMAIN = vercel_public_domain
   ? vercel_public_domain
-  : node_prod_env
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : vercel_public_url
+  ? `https://${vercel_public_url}`
   : 'http://127.0.0.1:3000';
