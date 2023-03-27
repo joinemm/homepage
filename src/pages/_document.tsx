a474ae7ddcd6a6f8c4f7543580e9af6c1db147ed
Joinemm <joonas@rautiola.co>
2023-03-27 12:54:14 +0300

    :chart_with_upwards_trend: Add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,11 @@ export default function Document() {
     <Html lang="en" className="text-[16px]">
       <Head>
         <style type="text/css" dangerouslySetInnerHTML={{ __html: mediaStyles }} />
+        <script
+          defer
+          data-domain="joinemm.dev"
+          src="https://pls.joinemm.dev/js/plausible.js"
+        ></script>
       </Head>
       <body className="bg-primary fg-primary max-w-[100vw] overflow-x-hidden">
         <Main />
