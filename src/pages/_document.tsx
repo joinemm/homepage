34369458e4cf6de24f0d765ede7d0f42105b3d92
Joinemm <joonas@rautiola.co>
2023-08-07 19:19:32 +0300

    :chart_with_upwards_trend: Fix analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { ANALYTICS_DOMAIN } from '../util/constants';
+import { ANALYTICS_DOMAIN, SITE_NAME } from '../util/constants';
 import { mediaStyles } from '../util/media-context';
 
 export default function Document() {
@@ -13,7 +13,7 @@ export default function Document() {
         <style type="text/css" dangerouslySetInnerHTML={{ __html: mediaStyles }} />
         <script
           defer
-          data-domain={ANALYTICS_DOMAIN}
+          data-domain={SITE_NAME}
           src={`https://${ANALYTICS_DOMAIN}/pls.js`}
         ></script>
       </Head>
