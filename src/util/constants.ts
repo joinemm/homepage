68ab03f5b4d8efeec31700bdaa82be459ff0a78f
Joinemm <joonas@rautiola.co>
2024-01-14 00:37:12 +0200

    :bug: Fix domain name

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,12 +1,6 @@
-const vercel_public_domain = process.env.NEXT_PUBLIC_DOMAIN;
-const vercel_public_url = process.env.NEXT_PUBLIC_VERCEL_URL;
-
-// if Vercel prod deployment, then use main domain; else use temporary domain unless you're on dev
-export const DOMAIN = vercel_public_domain
-  ? vercel_public_domain
-  : vercel_public_url
-  ? `https://${vercel_public_url}`
-  : 'http://127.0.0.1:3000';
+// configured in vercel env variables
+const vercel_url = process.env.VERCEL_URL;
+export const DOMAIN = vercel_url ? vercel_url : 'http://127.0.0.1:3000';
 
 export const PAGE_WIDTH = 40;
 export const CDN_DOMAIN = 'cdn.joinemm.dev';
